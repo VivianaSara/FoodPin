a5183d72d17023f6775d513172de05238d30da94
VivianaSara <viviana.mesaros@fortech.ro>
2022-04-13 12:00:34 +0300

    chapter9 challenge2

--- FoodPin/FoodPin/RestaurantTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -17,22 +17,27 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Aenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional","Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney","New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian/ Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee &Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
     func configureDataSource() -> UITableViewDiffableDataSource<Section, String >{
-        let cellIdentifier = "datacell"
+        let cellIdentifier = "favoritecell"
         let dataSource = UITableViewDiffableDataSource<Section, String>(tableView: tableView, cellProvider: { [self]
             tableView, indexPath, restaurantName in
             let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
-            cell.configureCell(nameLabel: restaurantName, locationLabel: "Location", typeLabel: "Type", thumbnailImageView: self.restaurantImages[indexPath.row])
+            cell.configureCell(nameLabel: restaurantName , locationLabel: restaurantLocations[indexPath.row], typeLabel: restaurantTypes[indexPath.row], thumbnailImageView: restaurantImages[indexPath.row])
 
             return cell
         }
     )
         return dataSource
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        tableView.separatorStyle = .none
+
         var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
         snapshot.appendSections([.all])
         snapshot.appendItems(restaurantNames, toSection: .all)

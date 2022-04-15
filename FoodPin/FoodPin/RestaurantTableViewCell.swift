863e323278ea68d4ff774735c04cb48c5362fc94
VivianaSara <viviana.mesaros@fortech.ro>
2022-04-15 12:23:56 +0300

    chapter10 + challenges

--- FoodPin/FoodPin/RestaurantTableViewCell.swift
+++ FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -8,17 +8,23 @@
 import UIKit
 
 class RestaurantTableViewCell: UITableViewCell {
-    
     @IBOutlet private var nameLabel: UILabel!
     @IBOutlet private var locationLabel: UILabel!
     @IBOutlet private var typeLabel: UILabel!
     @IBOutlet private var thumbnailImageView: UIImageView!
+    @IBOutlet private var heartImageView: UIImageView!
 
-    func configureCell(nameLabel: String, locationLabel: String, typeLabel: String, thumbnailImageView: String) -> Void{
+    func configureCell(nameLabel: String, locationLabel: String, typeLabel: String,
+                       thumbnailImageView: String) {
         self.thumbnailImageView.image = UIImage(named: thumbnailImageView)
         self.typeLabel.text = typeLabel
         self.locationLabel.text = locationLabel
         self.nameLabel.text = nameLabel
+        self.heartImageView.isHidden = true
+    }
+
+    func setHeartImage(isFavorite: Bool) {
+        self.heartImageView.isHidden = isFavorite
     }
 
     override func awakeFromNib() {

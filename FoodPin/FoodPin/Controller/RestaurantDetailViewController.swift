b8d60de9716a21053eb33d68ef62c10c14936678
VivianaSara <viviana.mesaros@fortech.ro>
2022-04-27 17:58:24 +0300

    commit chapter 14

--- FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -9,39 +9,59 @@ import UIKit
 
 class RestaurantDetailViewController: UIViewController {
 
-    @IBOutlet private var restaurantImageView: UIImageView!
-    @IBOutlet private var restaurantLocation: UILabel!
-    @IBOutlet private var restaurantType: UILabel!
-    @IBOutlet private var restaurantName: UILabel!
+    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var headerView: RestaurantDetailHeaderView!
+
     private var restaurant: Restaurant = Restaurant.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        navigationItem.largeTitleDisplayMode = .never
+        navigationController?.navigationBar.prefersLargeTitles = false
 
-        restaurantImageView.image = UIImage(named: restaurant.getImage())
-        restaurantName.text = restaurant.getName()
-        restaurantType.text = restaurant.getType()
-        restaurantLocation.text = restaurant.getLocation()
+        // Configure header view
+        headerView.configure(restaurant: restaurant)
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
     }
 
-    func getRestaurat() -> Restaurant {
-        return self.restaurant
+    func getRestaurat() -> Restaurant { return self.restaurant }
+    func setRestaurat(restaurant: Restaurant) { self.restaurant = restaurant }
+}
+
+extension RestaurantDetailViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
 
-    func setRestaurat(restaurant: Restaurant) {
-        self.restaurant = restaurant
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: RestaurantDetailTextCell.self),
+                for: indexPath) as? RestaurantDetailTextCell else {
+                return UITableViewCell()
+        }
+
+            cell.setDescriptionLabel(text: restaurant.getDescrption())
+            return cell
+
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(
+                withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self),
+                for: indexPath) as? RestaurantDetailTwoColumnCell else {
+                return UITableViewCell()
+            }
+
+            cell.configure(address: "Address", fullAddress: restaurant.getLocation(),
+                           phone: "Phone", phoneNumber: restaurant.getPhone())
+            return cell
+
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
